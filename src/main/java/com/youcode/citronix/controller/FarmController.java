04e8089601fcb5276f634a70ae65e20744361ca4
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-11-20 13:02:22 +0100

    CIT-9 Add multi_criteria search functionaliy

--- src/main/java/com/youcode/citronix/controller/FarmController.java
+++ src/main/java/com/youcode/citronix/controller/FarmController.java
@@ -1,13 +1,14 @@
 package com.youcode.citronix.controller;
 
 import com.youcode.citronix.dto.FarmDTO;
-import com.youcode.citronix.entity.Farm;
-import com.youcode.citronix.service.FarmService;
+import com.youcode.citronix.service.FarmServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -15,35 +16,47 @@ import java.util.List;
 public class FarmController {
 
     @Autowired
-    private FarmService farmService;
+    private FarmServiceImpl farmServiceImpl;
 
     @PostMapping
     public ResponseEntity<FarmDTO> createFarm(@RequestBody FarmDTO farmDTO){
-        FarmDTO createdFarm = farmService.createFarm(farmDTO);
+        FarmDTO createdFarm = farmServiceImpl.createFarm(farmDTO);
         return new ResponseEntity<>(createdFarm, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<FarmDTO>> getAllFarms(){
-        List<FarmDTO> farms = farmService.getAllFarms();
+        List<FarmDTO> farms = farmServiceImpl.getAllFarms();
         return new ResponseEntity<>(farms, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<FarmDTO> getFarmById(@PathVariable Long id){
-        FarmDTO farm = farmService.getFarmById(id);
+        FarmDTO farm = farmServiceImpl.getFarmById(id);
         return new ResponseEntity<>(farm, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<FarmDTO> updateFarm(@PathVariable Long id, @RequestBody FarmDTO farmDTO){
-        FarmDTO updatedFarm = farmService.updateFarm(id, farmDTO);
+        FarmDTO updatedFarm = farmServiceImpl.updateFarm(id, farmDTO);
         return new ResponseEntity<>(updatedFarm, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFarm(@PathVariable Long id){
-        farmService.deleteFarm(id);
+        farmServiceImpl.deleteFarm(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<FarmDTO>> searchFarms(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String location,
+            @RequestParam(required = false) Double minSize,
+            @RequestParam(required = false) Double maxSize,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate creationDate
+            ){
+        List<FarmDTO> farms = farmServiceImpl.searchFarms(name, location, minSize, maxSize, creationDate);
+        return new ResponseEntity<>(farms, HttpStatus.OK);
     }
 }
--- src/main/java/com/youcode/citronix/service/FarmService.java
+++ src/main/java/com/youcode/citronix/service/FarmService.java
@@ -1,51 +1,21 @@
 package com.youcode.citronix.service;
 
 import com.youcode.citronix.dto.FarmDTO;
-import com.youcode.citronix.entity.Farm;
-import com.youcode.citronix.mapper.FarmMapper;
-import com.youcode.citronix.repository.FarmRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class FarmService {
+public interface FarmService {
 
-    @Autowired
-    private FarmRepository farmRepository;
+    FarmDTO createFarm(FarmDTO farmDTO);
 
-    private final FarmMapper farmMapper = FarmMapper.INSTANCE;
+    List<FarmDTO> getAllFarms();
 
-    public FarmDTO createFarm(FarmDTO farmDTO) {
-        Farm farm = farmMapper.farmDTOToFarm(farmDTO);
-        Farm savedFarm = farmRepository.save(farm);
-        return farmMapper.farmToFarmDTO(savedFarm);
-    }
+    FarmDTO getFarmById(Long id);
 
-    public List<FarmDTO> getAllFarms() {
-        List<Farm> farms = farmRepository.findAll();
-        return farmMapper.farmsToFarmDTOs(farms);
-    }
+    FarmDTO updateFarm(Long id, FarmDTO farmDTO);
 
-    public FarmDTO getFarmById(Long id) {
-        Farm farm = farmRepository.findById(id).orElseThrow(() -> new RuntimeException("Farm not found"));
-        return farmMapper.farmToFarmDTO(farm);
-    }
+    void deleteFarm(Long id);
 
-    public FarmDTO updateFarm(Long id, FarmDTO farmDTO) {
-        Farm existingFarm = farmRepository.findById(id).orElseThrow(() -> new RuntimeException("Farm not found"));
-        existingFarm.setName(farmDTO.getName());
-        existingFarm.setLocation(farmDTO.getLocation());
-        existingFarm.setSize(farmDTO.getSize());
-        existingFarm.setCreationDate(farmDTO.getCreationDate());
-
-        Farm updatedFarm = farmRepository.save(existingFarm);
-        return farmMapper.farmToFarmDTO(updatedFarm);
-    }
-
-    public void deleteFarm(Long id) {
-        farmRepository.deleteById(id);
-    }
+    List<FarmDTO> searchFarms(String name, String location, Double minSize, Double maxSize, LocalDate creationDate);
 }
