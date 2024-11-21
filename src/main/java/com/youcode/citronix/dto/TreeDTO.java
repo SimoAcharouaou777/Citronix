0dda78b092fed1a3daebbb87abe629f552544ba9
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-11-21 16:19:50 +0100

    validation tree updated

--- src/main/java/com/youcode/citronix/dto/TreeDTO.java
+++ src/main/java/com/youcode/citronix/dto/TreeDTO.java
@@ -1,5 +1,8 @@
 package com.youcode.citronix.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +13,12 @@ import java.time.LocalDate;
 public class TreeDTO {
 
     private Long id;
+    @NotNull(message = "Field id cannot be null")
     private Long fieldId;
+    @NotNull(message = "Tree type cannot be null")
+    @PastOrPresent(message = "Planting date must be in the past or present")
     private LocalDate plantingDate;
+    @Min(value = 0, message = "Tree age must be at least 0 and cannot be negative")
     private Integer age;
     private Double productivityRate;
 }
